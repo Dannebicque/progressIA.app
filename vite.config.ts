05828c8f321cf631582235cb7a4d954d16d7bcb5
davidannebicque <david.annebicque@gmail.com>
2026-08-31 12:03:54 +0200

    Release-please

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
+import { readFileSync } from 'node:fs'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import basicSsl from "@vitejs/plugin-basic-ssl"
+
+const packageJson = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +17,9 @@ export default defineConfig({
     tailwindcss(),
     basicSsl() // Active le HTTPS automatiquement
   ],
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version),
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
